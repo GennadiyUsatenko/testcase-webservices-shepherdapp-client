b8e5aaf348adfec7079659e21dfa650c9146cf65
GenaPC <usatenko.gena@gmail.com>
2018-09-23 12:11:18 +0300

    order new sheep

--- src/main/java/com/paazl/rest/ShepherdClient.java
+++ src/main/java/com/paazl/rest/ShepherdClient.java
@@ -5,8 +5,20 @@ import org.springframework.stereotype.Component;
 
 import com.paazl.gui.GuiInterface;
 
+import javax.ws.rs.client.*;
+import javax.ws.rs.ProcessingException;
+import javax.ws.rs.core.UriBuilder;
+import java.net.URI;
+
 @Component
 public class ShepherdClient {
+
+    private static final Client client = ClientBuilder.newClient();
+    private static final WebTarget target = client.target(getBaseURI());
+
+    private static URI getBaseURI() {
+        return UriBuilder.fromUri("http://localhost:8080/rest/shepherdmanager").build();
+    }
 
     /*
         TODO Use a Rest client to obtain the server status, so this client can be used to obtain that status.
@@ -14,10 +26,26 @@ public class ShepherdClient {
      */
     @Autowired
     public ShepherdClient(GuiInterface guiInterface) {
-        guiInterface.addOrderRequestListener(i -> guiInterface.addServerFeedback("Number of sheep to order: " + i));
+        guiInterface.addOrderRequestListener(
+                i -> {
+                    if(i < 1)guiInterface.addServerFeedback("Number of sheep must be greater than 0!");
+                    else {
+                        try {
+                            guiInterface.addServerFeedback(
+                                    target.path("order").path(Integer.toString(i)).request().post(null, String.class));
+                        }catch (ProcessingException e){
+                            guiInterface.addServerFeedback(e.getMessage());
+                        }
+                    }
+                }
+        );
     }
 
     public String getServerStatus() {
-        return "Server status...";
+        try {
+            return target.path("status").request().get(String.class);
+        }catch (ProcessingException e){
+            return e.getMessage();
+        }
     }
 }
